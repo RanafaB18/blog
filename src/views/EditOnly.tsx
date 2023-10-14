e4068ddf4fb7874739237f72c6ef63299d5a2d0a
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-10-14 13:36:26 +0000

    Fixed grid

--- src/views/EditOnly.tsx
+++ src/views/EditOnly.tsx
@@ -1,22 +1,15 @@
 import EditPost from "../components/EditPost";
 import { IForm } from "../interface";
 
-const EditOnly = ({posts}:{posts: IForm[]}) => {
+const EditOnly = ({ posts }: { posts: IForm[] }) => {
   return (
-    <div className="flex flex-col gap-4 xl:max-w-7xl xl:mx-auto xl:grid xl:grid-cols-3 ">
+    <div className="flex flex-col gap-4 xl:max-w-7xl xl:mx-auto xl:grid xl:grid-flow-row-dense xl:grid-cols-3">
       {posts.map((post) => {
-          const { title, excerpt, id } = post;
-          return (
-            <EditPost
-              key={id}
-              title={title}
-              excerpt={excerpt}
-              id={id}
-            />
-          );
-        })}
+        const { title, excerpt, id } = post;
+        return <EditPost key={id} title={title} excerpt={excerpt} id={id} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default EditOnly
+export default EditOnly;
